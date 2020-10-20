9475f214648de207fe9f2a87cd191487ffdff16f
Dyral <dyral.nguyen@gmail.com>
2020-10-20 20:23:51 +0700

    Add Country picker and changed data on chards

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changableUrl = url;
+    if (country) {
+        changableUrl = `${url}/countries/${country}`
+    }
+
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changableUrl);
         return { confirmed, recovered, deaths, lastUpdate };
 
     } catch (error) {
@@ -17,7 +22,16 @@ export const fetchDailyData = async () => {
         const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
         return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
-        // return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries } } = await axios.get(`${url}/countries`);
+        
+        return countries.map((country) => country.name);
     } catch (error) {
         return error;
     }
